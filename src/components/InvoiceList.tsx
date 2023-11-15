aa705d7b9923b56fd1668c0ba9eb7dd9de34c71b
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2023-11-16 03:58:23 +0400

    add InvoiceListHeader component

--- src/components/InvoiceList.tsx
+++ src/components/InvoiceList.tsx
@@ -1,7 +1,34 @@
+import styled from "styled-components";
 import InvoiceListItem from "./InvoiceListItem";
+import InvoiceListHeader from "./InvoiceListHeader";
+import DummyData from "../data.json";
 
 function InvoiceList() {
-  return <InvoiceListItem />;
+  return (
+    <>
+      <StyledList>
+        <InvoiceListHeader itemCount={DummyData.length} />
+        {DummyData.map((invoice) => {
+          return <InvoiceListItem key={invoice.id} {...invoice} />;
+        })}
+      </StyledList>
+    </>
+  );
 }
 
 export default InvoiceList;
+
+const StyledList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1.6rem;
+  width: 32.7rem;
+  @media (min-width: 48em) {
+    width: 67.2rem;
+  }
+  @media (min-width: 90em) {
+    width: 73rem;
+  }
+`;
