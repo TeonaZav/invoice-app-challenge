0e533b6107c3027b547400fa54a9a409c3fe8edd
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2023-11-17 22:47:25 +0400

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import InvoiceList from "./components/InvoiceList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Invoice from "./pages/Invoice";
 import GlobalStyles from "./styles/GlobalStyles";
-import Header from "./components/UI/Header";
+
+import AppLayOut from "./components/UI/AppLayout";
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Header />
-      <main>
-        <InvoiceList />
-      </main>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayOut />}>
+            <Route index path="/" element={<Home />}></Route>
+            <Route path="/invoice/:id" element={<Invoice />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
