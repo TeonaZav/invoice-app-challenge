875a76bba74f068664779e931f368b46415bddc8
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2023-11-29 23:24:38 +0400

    feat: add formContex and reducer

---
+++ src/context/formContext.tsx
@@ -0,0 +1,18 @@
+import { createContext, useContext, useReducer } from "react";
+import { initialState } from "../reducer";
+
+export const InvoiceFormContext = createContext(initialState);
+
+export const InvoiceFormProvider = ({
+  reducer,
+  initialState,
+  children,
+}: any) => {
+  return (
+    <InvoiceFormContext.Provider value={useReducer(reducer, initialState)}>
+      {children}
+    </InvoiceFormContext.Provider>
+  );
+};
+
+export const useInvoiceContextForm = () => useContext(InvoiceFormContext);
---
+++ src/reducer.tsx
@@ -0,0 +1,66 @@
+export const initialState = {
+  isEditSession: false,
+  drawerIsOpen: false,
+  default: {
+    createdAt: "",
+    paymentDue: "",
+    description: "",
+    paymentTerms: null,
+    clientName: "",
+    clientEmail: "",
+    status: "draft",
+    senderAddress: {
+      street: "",
+      city: "",
+      postCode: "",
+      country: "",
+    },
+    clientAddress: {
+      street: "",
+      city: "",
+      postCode: "",
+      country: "",
+    },
+    items: [
+      {
+        id: "",
+        name: "",
+        quantity: 1,
+        price: 0,
+        total: 0,
+      },
+    ],
+    total: 0,
+  },
+};
+
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "EDIT_FORM":
+      return {
+        ...state,
+        default: { ...action.item },
+        drawerIsOpen: action.drawer,
+        isEditSession: true,
+      };
+    case "CLOSE_DRAWER":
+      return {
+        ...state,
+        drawerIsOpen: false,
+      };
+    case "OPEN_DRAWER":
+      return {
+        ...state,
+        drawerIsOpen: true,
+      };
+    case "TOGGLE_DRAWER":
+      return {
+        ...state,
+        drawerIsOpen: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
